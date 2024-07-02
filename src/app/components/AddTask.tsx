59d953784008bb230bae015edc470445725e730d
itsstephenm <159146059+itsstephenm@users.noreply.github.com>
2024-07-02 23:29:22 +0200

    Title, Button, Table

---
+++ src/app/components/AddTask.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+
+const AddTask = () => {
+  return (
+    <div>
+      <button className='btn btn-primary w-full'>ADD NEW TASK <AiOutlinePlus size={13} /> </button>
+    </div>
+  )
+}
+
+export default AddTask
